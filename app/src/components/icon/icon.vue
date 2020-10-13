2c65b0278cb314d8196ddd29a2de17dae07e82a6
old-boy <kevin.chen@anviz.com>
2020-10-13 20:20:12 +0800

    图标

---
+++ app/src/components/icon/icon.vue
@@ -0,0 +1,32 @@
+<template>
+    <view class="icon-warp" @click="clickIcon" :backgroud="backgroud">
+        <image :size="size" :color="color" :src="src" class="icon"></image>
+    </view>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            size:String,
+            color:String,
+            src:String,
+            backgroud:String
+        }
+    },
+    methods: {
+        clickIcon(){
+            
+        }
+    },
+}
+</script>
+<style lang="scss" scoped>
+    .icon-warp{
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        .icon{
+            width: 50px;
+        }
+    }
+</style>
